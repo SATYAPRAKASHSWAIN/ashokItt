6d69201d47734cacc987e58be553a67a931df6dc
SATYAPRAKASHSWAIN <satyaprakashswain1998@gmail.com>
2024-03-02 11:58:00 +0530

    Revert "this is the second time commited !"
    This reverts commit d1a49f024452f0305951e2b0344e13f7f5a27b7e.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,9 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-      
-        String name="Satyaprakash Swain";
-        String name1="Ajit Kumar Jena";
         System.out.println( "Hello World!" );
     }
 }
